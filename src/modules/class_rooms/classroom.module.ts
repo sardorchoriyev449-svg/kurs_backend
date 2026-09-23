fbe665aebe3f376147d93f53e18c5e5de104449a
sardorthe17 <sardorthe17@gmail.com>
2026-09-23 10:22:58 +0500

    fix 5

--- src/modules/class_rooms/classroom.module.ts
+++ src/modules/class_rooms/classroom.module.ts
@@ -4,13 +4,15 @@ import { ClassRoomController } from './classroom.controller';
 import { ClassRoomService } from './classroom.service';
 import { ClassRoom, ClassRoomSchema } from './model/classroom.model';
 import { GroupModule } from '../groups/group.module';
+import { Group, GroupSchema } from '../groups/model/group.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ClassRoom.name, schema: ClassRoomSchema },
+      { name: Group.name, schema: GroupSchema },
     ]),
-    GroupModule,
+    
   ],
   controllers: [ClassRoomController],
   providers: [ClassRoomService],
