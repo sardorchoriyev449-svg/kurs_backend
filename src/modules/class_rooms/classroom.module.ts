02a05229e7089a2f12ab68e0e0831a7ce47fb4e7
sardorthe17 <sardorthe17@gmail.com>
2026-09-23 10:20:35 +0500

    ...

--- src/modules/class_rooms/classroom.module.ts
+++ src/modules/class_rooms/classroom.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ClassRoomController } from './classroom.controller';
 import { ClassRoomService } from './classroom.service';
 import { ClassRoom, ClassRoomSchema } from './model/classroom.model';
+import { GroupModule } from '../groups/group.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ClassRoom.name, schema: ClassRoomSchema },
     ]),
+    GroupModule,
   ],
   controllers: [ClassRoomController],
   providers: [ClassRoomService],
